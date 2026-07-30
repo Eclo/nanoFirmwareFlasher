77bd02346ac9cce80ed60205dea3354f2afdd8e7
Laurent Ellerbach <laurelle@microsoft.com>
2026-07-30 19:24:18 +0200

    Fix STM32 JTAG flash and improve overall STM32 flashing (#363)
    (cherry picked from commit baccab8dd8309aa58af4c7287bb2f866292fb22d)

--- nanoFirmwareFlasher.Tool/Program.cs
+++ nanoFirmwareFlasher.Tool/Program.cs
@@ -613,6 +613,8 @@ namespace nanoFramework.Tools.FirmwareFlasher
                 // JTAG related
                 if (
                     o.ListJtagDevices ||
+                    o.ListNativeStLinkDevices ||
+                    o.ListNativeSwdDevices ||
                     !string.IsNullOrEmpty(o.JtagDeviceId) ||
                     o.HexFile.Any() ||
                     o.BinFile.Any())
@@ -622,6 +624,7 @@ namespace nanoFramework.Tools.FirmwareFlasher
                 // DFU related
                 else if (
                     o.ListDevicesInDfuMode ||
+                    o.ListNativeDfuDevices ||
                     o.DfuUpdate ||
                     !string.IsNullOrEmpty(o.DfuDeviceId))
                 {
@@ -638,8 +641,7 @@ namespace nanoFramework.Tools.FirmwareFlasher
                     o.Platform = SupportedPlatform.ti_simplelink;
                 }
                 else if (
-                    o.InstallDfuDrivers
-                    || o.InstallJtagDrivers)
+                    o.InstallJtagDrivers)
                 {
                     o.Platform = SupportedPlatform.stm32;
                 }
